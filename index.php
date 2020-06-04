feab652fa4c8999793b1a3c293c2ab32c603b8a2
morimotohirotomo <minamoyori19@outlook.jp>
2020-06-04 23:46:42 +0900

    add index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>quiz</title>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+  <div id="container">
+  	<h1>Q. What is A?</h1>
+  	<ul>
+  	  <li class="answer">A0</li>
+  	  <li class="answer">A1</li>
+  	  <li class="answer">A2</li>
+  	  <li class="answer">A3</li>
+  	</ul>
+  	<div id="btn" class="disabled">
+  	  Next Question
+  	</div>
+  </div>
+</body>
+</html>
